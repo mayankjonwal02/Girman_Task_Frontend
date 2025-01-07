c6427ecf4551205b40712c384c66d28dcd9f798d
mayankjonwal02 <jonwal.1@iitj.ac.in>
2025-01-07 23:30:41 +0530

    changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 export const fetchUserData = async () => {
-    const response = await fetch("http://127.0.0.1:8000/api/userdata/");
+    const response = await fetch("https://girmantaskbackend.vercel.app/api/userdata/");
     console.log(response);
     return response.json();
   };
   
   export const postUserData = async (data) => {
-    const response = await fetch("http://127.0.0.1:8000/api/userdata/", {
+    const response = await fetch("https://girmantaskbackend.vercel.app/api/userdata/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
