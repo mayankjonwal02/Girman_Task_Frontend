35a8e12422e19b1cfbbe78f1e89f9fa01a268f01
mayankjonwal02 <jonwal.1@iitj.ac.in>
2025-01-12 02:38:53 +0530

    new global

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,14 @@
 const localhost = "http://127.0.0.1:8000";
-const global = "https://girmantaskbackend.vercel.app";
+const globalhost = "https://girmantaskbackend.vercel.app";
 
 export const fetchUserData = async () => {
-    const response = await fetch(localhost+"/api/userdata/");
+    const response = await fetch(globalhost+"/api/userdata/");
     console.log(response);
     return response.json();
   };
   
 export const postUserData = async (data) => {
-    const response = await fetch(localhost+"/api/userdata/", {
+    const response = await fetch(globalhost+"/api/userdata/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
@@ -17,7 +17,7 @@ export const postUserData = async (data) => {
   };
 
   export const fetchUserDataByName = async (name) => {
-    const response = await fetch(localhost+"/api/userdata_byname/"+name);
+    const response = await fetch(globalhost+"/api/userdata_byname/"+name);
     console.log(response);
     return response.json();
   };
